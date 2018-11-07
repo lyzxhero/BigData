6b8bf290f89ab7ca993f45129d38cb6060f96ea6
hero.li <liyaohui@juxiangfen.com>
2018-11-07 11:01:45 +0800

    one

---
+++ src/main/java/com/lyzx/kafka/consumer/T1.java
@@ -0,0 +1,38 @@
+package com.lyzx.kafka.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.junit.Test;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class T1 {
+
+    @Test
+    public void test1() throws IOException {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "172.16.124.92:9092,172.16.124.93:9092,172.16.124.94:9092");
+        props.put("group.id", "test");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("front-event"));
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter(""));
+
+        while(true){
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for(ConsumerRecord<String, String> record : records){
+                System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+            }
+
+        }
+    }
+}
